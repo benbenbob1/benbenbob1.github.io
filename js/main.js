bf3623488c92d2ab19157b4cd704275f89d44168
Ben <benbrown52@gmail.com>
2018-10-03 00:00:01 -0700

    Updated secret, small version bump

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {
     var siteName = "benbrown.science";
     var shortName = "bbs";
-    var version = 0.961;
+    var version = 0.965;
 
     /*                                            *
      *    Feel free to look around!               *
